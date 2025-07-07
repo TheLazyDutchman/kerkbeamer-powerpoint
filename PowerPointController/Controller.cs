adfd7669138242c6d3d5c1107b34ed079b01739a
jochem pouwels <jjpouwels2004@gmail.com>
2025-07-07 14:40:19 +0200

    Add c# powerpoint controller

---
+++ PowerPointController/Controller.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Office.Interop.PowerPoint;
+using WebSocketSharp.Server;
+using WebSocketSharp;
+using Microsoft.Office.Core;
+
+namespace PowerPointController
+{
+    public class Controller
+    {
+        private Application _app;
+        private Presentation _presentation;
+        private SlideShowWindow _slideshow;
+        private WebSocketServer _wss;
+
+        public void Start(string path)
+        {
+            // Start WebSocket server
+            _wss = new WebSocketServer(8181);
+            _wss.AddWebSocketService<PowerPointWebSocket>("/ppt");
+            _wss.Start();
+
+            // Start PowerPoint
+            _app = new Application();
+            _app.Visible = MsoTriState.msoTrue;
+
+            // Open presentatie
+            _presentation = _app.Presentations.Open(path,
+                WithWindow: MsoTriState.msoTrue);
+
+            // Start slideshow en registreer event
+            var settings = _presentation.SlideShowSettings;
+            settings.ShowWithNarration = MsoTriState.msoFalse;
+            settings.ShowWithAnimation = MsoTriState.msoTrue;
+            _slideshow = settings.Run();
+            _slideshow.View.GotoSlide(1); // naar begin
+
+            _app.SlideShowNextSlide += OnNextSlide;
+        }
+
+        private void OnNextSlide(SlideShowWindow Wn)
+        {
+            int currentSlide = Wn.View.CurrentShowPosition;
+            PowerPointWebSocket.BroadcastSlideChanged(currentSlide);
+        }
+
+        public void Stop()
+        {
+            _presentation?.Close();
+            _app?.Quit();
+            _wss?.Stop();
+        }
+    }
+
+    public class PowerPointWebSocket : WebSocketBehavior
+    {
+        private static WebSocketSessionManager _sessions;
+
+        protected override void OnOpen()
+        {
+            _sessions = Sessions;
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            if (e.Data == "next")
+            {
+                var app = new Application();
+                var view = app?.SlideShowWindows?[1]?.View;
+                view?.Next();
+            }
+            else if (e.Data.StartsWith("goto:"))
+            {
+                int slideNum = int.Parse(e.Data.Split(':')[1]);
+                var app = new Application();
+                var view = app?.SlideShowWindows?[1]?.View;
+                view?.GotoSlide(slideNum);
+            }
+        }
+
+        public static void BroadcastSlideChanged(int slideNumber)
+        {
+            _sessions?.Broadcast($"slide:{slideNumber}");
+        }
+    }
+}
---
+++ PowerPointController/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PowerPointController
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var controller = new Controller();
+            controller.Start(args[0]);
+            Console.WriteLine("PowerPoint bridge gestart. Druk op Enter om te stoppen...");
+            Console.ReadLine();
+            controller.Stop();
+        }
+    }
+}
